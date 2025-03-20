ecba7151d6ff4cbd91627cda3975437896e393c2
sickteenx <bestzerzr@gmail.com>
2025-03-20 22:39:37 +0700

    two

--- Assets/Scripts/RotateF.cs
+++ Assets/Scripts/RotateF.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class RotateF : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody rb;
+    [SerializeField] Vector3 angularV, torque;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.angularVelocity = angularV;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            rb.AddTorque(torque);
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
     }
 }
